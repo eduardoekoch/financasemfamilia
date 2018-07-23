04d5d14d70fd6a1ac0a5ff784a61bd37c85f0218
Marcos Vieira <mavbvieira@gmail.com>
2018-07-23 14:26:16 -0300

    [WIP] Implementa i18n no usuario

--- spec/features/visitor_sign_up_spec.rb
+++ spec/features/visitor_sign_up_spec.rb
@@ -8,8 +8,12 @@ feature 'visitor_sign_up' do
     click_on 'Sign up'
 
     fill_in 'Email', with: 'teste@gmail.com'
-    fill_in 'Senha', with: '12345678'
+    fill_in 'Senha', with: '123456'
+    fill_in 'Confirmar', with: '123456'
+    click_on 'Sign up'
 
     expect(page).to have_content('Usuário cadastrado com sucesso')
+    expect(page).to have_content('teste@gmail.com')
+    expect(page).not_to have_content('Cadastro')
   end
 end
